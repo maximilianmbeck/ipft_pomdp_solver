963147951f0f7f2d8d42d214828f3e1986ad0f8b
Maximilian Beck <mbeck@mrt.kit.edu>
2021-01-18 08:33:10 +0100

    dummy implementations for point interface added

--- include/ipft/interface/point.hpp
+++ include/ipft/interface/point.hpp
@@ -15,70 +15,32 @@ class Point : public MemoryObject {
     Point(){};
     virtual ~Point(){};
 
-    virtual bool equals(const Point& p) const = 0;
+    virtual bool equals(const Point& p) const;
 
-    // virtual double distanceTo(const Point& p) const = 0;
+    virtual int dimensions() const;
 
-    virtual int dimensions() const = 0;
+    virtual double get(int dim) const;
 
-    virtual double get(int dim) const = 0;
+    virtual void set(const double& p, int dim);
 
-    virtual void set(const double& p, int dim) = 0;
-
-    virtual void reset() = 0;
-
-    // virtual bool operator<(const Point& rhs) const = 0;
-
-    // virtual bool operator==(const Point& rhs) const = 0;
+    virtual void reset();
 
     // don't do this Points should be only allocated through the POMDP model
     // virtual Point* clone() const = 0;
 
-    virtual std::string text() const = 0;
+    virtual std::string text() const;
 
     friend std::ostream& operator<<(std::ostream& os, const Point& p) {
         os << p.text();
         return os;
     }
 
-    static std::vector<double> mean(const std::vector<Point*>& points) {
-        CHECK(points.size() > 0) << "Point set empty";
+    static std::vector<double> mean(const std::vector<Point*>& points);
 
-        int dimensions = points[0]->dimensions();
-        std::vector<double> meanVec(dimensions);
-
-        for (int dim = 0; dim < dimensions; dim++) {
-            double dimValue = 0.0;
-            for (int i = 0; i < points.size(); i++) {
-                dimValue += points[i]->get(dim);
-            }
-            dimValue = dimValue / points.size();
-            meanVec[dim] = dimValue;
-        }
-        return meanVec;
-    }
-
-    static std::vector<double> variance(const std::vector<Point*>& points) {
-        std::vector<double> meanVec = Point::mean(points);
-        CHECK(points.size() > 1) << "Need more than one particle to compute a variance";
-        int dimensions = points[0]->dimensions();
-        std::vector<double> varianceVec(dimensions);
-
-        for (int dim = 0; dim < dimensions; dim++) {
-            double dimVarianceValue = 0.0;
-            for (int i = 0; i < points.size(); i++) {
-                double x = points[i]->get(dim) - meanVec[dim];
-                dimVarianceValue += x * x;
-            }
-            // this is how var() is implemented in julia -> in literature: also (points.size()-1) (correction factor)
-            dimVarianceValue = dimVarianceValue / (points.size());
-            varianceVec[dim] = dimVarianceValue;
-        }
-        return varianceVec;
-    }
+    static std::vector<double> variance(const std::vector<Point*>& points);
 
    private:
-    virtual double epsilon() const = 0;
+    virtual double epsilon() const;
 };
 
 }  // namespace solver_ipft
---
+++ src/point.cpp
@@ -0,0 +1,70 @@
+#include <glog/logging.h>
+
+#include <cmath>
+#include <ipft/interface/point.hpp>
+
+namespace solver_ipft {
+
+bool Point::equals(const Point& p) const {
+    return false;
+}
+
+int Point::dimensions() const {
+    return -1;
+}
+
+double Point::get(int dim) const {
+    return std::nan("");
+}
+
+void Point::set(const double& p, int dim) {
+}
+
+void Point::reset() {
+}
+
+std::string Point::text() const {
+    return std::string("Not implemented");
+}
+
+std::vector<double> Point::mean(const std::vector<Point*>& points) {
+    CHECK(points.size() > 0) << "Point set empty";
+
+    int dimensions = points[0]->dimensions();
+    std::vector<double> meanVec(dimensions);
+
+    for (int dim = 0; dim < dimensions; dim++) {
+        double dimValue = 0.0;
+        for (int i = 0; i < points.size(); i++) {
+            dimValue += points[i]->get(dim);
+        }
+        dimValue = dimValue / points.size();
+        meanVec[dim] = dimValue;
+    }
+    return meanVec;
+}
+
+std::vector<double> Point::variance(const std::vector<Point*>& points) {
+    std::vector<double> meanVec = Point::mean(points);
+    CHECK(points.size() > 1) << "Need more than one particle to compute a variance";
+    int dimensions = points[0]->dimensions();
+    std::vector<double> varianceVec(dimensions);
+
+    for (int dim = 0; dim < dimensions; dim++) {
+        double dimVarianceValue = 0.0;
+        for (int i = 0; i < points.size(); i++) {
+            double x = points[i]->get(dim) - meanVec[dim];
+            dimVarianceValue += x * x;
+        }
+        // this is how var() is implemented in julia -> in literature: also (points.size()-1) (correction factor)
+        dimVarianceValue = dimVarianceValue / (points.size());
+        varianceVec[dim] = dimVarianceValue;
+    }
+    return varianceVec;
+}
+
+double Point::epsilon() const {
+    return std::nan("1");
+}
+
+}  // namespace solver_ipft
