f4439bed7b8d1a2ae6d48cf4d4eaf0201fc68597
Sahin Tas <sahin.tas@kit.edu>
2021-05-20 12:23:30 +0200

    Revise documentation

--- include/solver_ipft/interface/pomdp.hpp
+++ include/solver_ipft/interface/pomdp.hpp
@@ -40,31 +40,83 @@ public:
      */
     virtual std::unique_ptr<Belief> initialBelief(const std::string& type);
 
+    /**
+     * @brief Transition model of the pomdp.
+     *
+     * @param state     Prior state
+     * @param action    Action applied in prior state
+     * @return State*   Generated posterior state
+     */
     virtual State* transition(const State& state, const Action& action) const = 0;
 
+    /**
+     * @brief Observation model of the pomdp.
+     *  Depends only on the posterior state
+     * @param statePosterior    Posterior state
+     * @return Observation*     Generated observation
+     */
     virtual Observation* observation(const State& statePosterior) const = 0;
 
+    /**
+     * @brief Observation probability factor used for particle weighting.
+     * The result is not a probability; it needs to be normalized.
+     * @param statePosterior
+     * @param obs               Observation
+     * @return double           Observation probability factor
+     */
     virtual double obsProb(const State& statePosterior, const Observation& obs) const;
 
+    /**
+     * @brief Observation probability factor used for particle weighting.
+     * The result is not a probability; it needs to be normalized.
+     * @param state             Prior state
+     * @param action            Prior action
+     * @param statePosterior    Posterior state
+     * @param obs               Observation
+     * @return double           Observation probability factor
+     */
     virtual double obsProb(const State& state,
                            const Action& action,
                            const State& statePosterior,
                            const Observation& obs) const;
 
-    virtual double maxPossibleWeight(const Action& act, const Observation& obs) const;
+    /**
+     * @brief Returns the maximum value of the observation probability distribution.
+     * Returns the maximum value obsProb can return.
+     * @param action            Prior action
+     * @param obs               Observation
+     * @return double           Maximum observation probability factor
+     */
+    virtual double maxPossibleWeight(const Action& action, const Observation& obs) const;
 
     /**
      * @brief Returns the reward for taking an action at a state
      *
-     * @param state     Current state of the world
-     * @param action    Action to be taken
+     * @param state     Prior state
+     * @param action    Prior action
      * @return double   Reward for taking action in state
      */
     virtual double reward(const State& state, const Action& action) const;
 
+    /**
+     * @brief Returns the reward for taking an action at state and transition to state posterior.
+     * Can consider the reward associated with statePosterior as well, based on structure of the pomdp model.
+     * @param state             Prior state
+     * @param action            Prior action
+     * @param statePosterior    Posterior state
+     * @return double           Reward for taking action in prior state and transition to posterior state
+     */
     virtual double reward(const State& state, const Action& action, const State& statePosterior) const;
 
-    virtual bool terminalState(const State& statePosterior, const Action& action) const = 0;
+    /**
+     * @brief Check if prior action leads to terminal state.
+     *
+     * @param action            Prior action
+     * @param statePosterior    Posterior state
+     * @return true             Terminal state reached
+     * @return false            Terminal state not reached
+     */
+    virtual bool terminalState(const Action& action, const State& statePosterior) const = 0;
 
     /**
      * @brief Returns number of actions.
@@ -73,42 +125,34 @@ public:
     virtual int numActions() const = 0;
 
     /**
-     * @brief Function mapping an action(index) to the action value
+     * @brief Function mapping an action(index) to the action value.
      * Must be defined by the concrete model. ActionValue object is a Point
      * @param act the action index
-     * @return ActionValue& the action value (concrete type must be defined with
-     * the concrete model)
+     * @return ActionValue& the action value (concrete type must be defined with the concrete model)
      */
     virtual std::unique_ptr<ActionValue> valueOfAction(const Action& act) const;
 
     /**
      * @brief Returns the (current) number of dimensions of the state space.
-     * "current" means: the dimension of the states to be created next, if model
-     * does not change.
+     * "current" means: the dimension of the states to be created next.
      * @return int number of dimensions of the state space
      */
     virtual int numDimStateSpace() const;
 
     /**
-     * @brief Returns states in the near of the State state. Used for particle
-     * reinvigoration.
-     *
-     * @param state the state around all the other states are distributed (the
-     * state is copied internally)
-     * @param count the number of similar states to return (count > 0, if count =
-     * 1 then the set of similar states only contains the parameter state)
-     * @return std::vector<State*> the set of similar states including the
-     * parameter state
+     * @brief Returns states in the vicinity of the state.
+     * Used for particle reinvigoration.
+     * @param state the state around all the other states are distributed (the state is copied internally)
+     * @param count the number of similar states to return (count > 0).
+     * @return std::vector<State*> the set of similar states including the parameter state
      */
     virtual std::vector<State*> similarStates(const State& state, int count) const;
 
     /**
-     * @brief Replaces @param particle with a resampled particle around the
-     * current observation @param obs
-     *
+     * @brief Replaces @param particle with a resampled particle in the vicinity of the current observation @param obs
+     * Used for particle reinvigoration in ObsAdaptiveReinvigorator.
      * @param particle the particle to replace
-     * @param particleSet the unweighted / resampled particle set --> all weights
-     * must be 1/N_particles
+     * @param particleSet the unweighted / resampled particle set --> all weights must be 1/N_particles
      * @param act current action
      * @param obs current observation
      */
